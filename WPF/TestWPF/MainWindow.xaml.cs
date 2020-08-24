566ff9eee86150bc265a85022680c44353734502
MastProgs <khjkhj2804@naver.com>
2020-08-24 16:13:46 +0900

    added WPF test

---
+++ WPF/TestWPF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestWPF
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class HelloWPF : Window
+    {
+        public HelloWPF()
+        {
+            InitializeComponent();
+        }
+
+        private void okBtn_Click(object sender, RoutedEventArgs e)
+        {
+            logMonitor.Text = "Ok Button Clicked";
+        }
+
+        private void cancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            logMonitor.Text = "Cancel Button Clicked";
+        }
+    }
+}
